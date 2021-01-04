be89fca7c5752ac5e51ba95fd97b8b5a40c987ab
Vaibhav Arora <varora1406@gmail.com>
2021-01-05 00:46:11 +0530

    feat: 🎸 switch from local code to pwa-helper-component
    BREAKING CHANGE: 🧨 complete Input contract is changed

--- build/pwa-update.d.ts
+++ build/pwa-update.d.ts
@@ -1,18 +1,15 @@
 import { LitElement } from 'lit-element';
+import 'pwa-helper-components/pwa-update-available.js';
 export declare class pwaupdate extends LitElement implements PWAUpdateComponent {
-    swpath: string;
-    updateevent: string;
     updatemessage: string;
-    readyToAsk: boolean;
-    showStorageEstimate: boolean;
-    showOfflineToast: boolean;
-    offlineToastDuration: number;
-    storageUsed: string | null;
-    swreg: ServiceWorkerRegistration | undefined;
+    show: boolean;
+    static get properties(): {
+        show: {
+            type: BooleanConstructor;
+            reflect: boolean;
+        };
+    };
     static get styles(): import("lit-element").CSSResult;
-    firstUpdated(): Promise<void>;
-    setupEvents(): void;
-    doUpdate(): void;
-    formatBytes(bytes: number, decimals?: number): string;
+    private _updateShow;
     render(): import("lit-element").TemplateResult;
 }
--- build/pwa-update.js
+++ build/pwa-update.js
@@ -4,18 +4,18 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-import { LitElement, html, customElement, css, property } from 'lit-element';
+import { LitElement, html, customElement, css, property, eventOptions } from 'lit-element';
+import 'pwa-helper-components/pwa-update-available.js';
 let pwaupdate = class pwaupdate extends LitElement {
     constructor() {
         super(...arguments);
-        this.swpath = "pwabuilder-sw.js";
-        this.updateevent = "SKIP_WAITING";
         this.updatemessage = "An update for this app is available";
-        this.readyToAsk = false;
-        this.showStorageEstimate = false;
-        this.showOfflineToast = false;
-        this.offlineToastDuration = 2400;
-        this.storageUsed = null;
+        this.show = false;
+    }
+    static get properties() {
+        return {
+            show: { type: Boolean, reflect: true }
+        };
     }
     static get styles() {
         return css `
@@ -42,6 +42,11 @@ let pwaupdate = class pwaupdate extends LitElement {
 
         animation-name: fadein;
         animation-duration: 300ms;
+        display: none;
+      }
+
+      #updateToast[show='true'] {
+        display: block;
       }
 
       #storageToast {
@@ -85,127 +90,34 @@ let pwaupdate = class pwaupdate extends LitElement {
           opacity: 1;
         }
       }
+
+
     `;
     }
-    async firstUpdated() {
-        var _a, _b;
-        if (this.swpath) {
-            if ('serviceWorker' in navigator) {
-                const reg = await navigator.serviceWorker.register(this.swpath);
-                let worker = reg.installing;
-                if (worker) {
-                    if (navigator.storage) {
-                        const storageData = await navigator.storage.estimate();
-                        if (storageData) {
-                            this.storageUsed = this.formatBytes(storageData.usage);
-                            this.showOfflineToast = true;
-                            await this.updateComplete;
-                            const ani = (_b = (_a = this.shadowRoot) === null || _a === void 0 ? void 0 : _a.querySelector('#storageToast')) === null || _b === void 0 ? void 0 : _b.animate([
-                                {
-                                    opacity: 0
-                                },
-                                {
-                                    opacity: 1
-                                }
-                            ], {
-                                fill: 'forwards',
-                                duration: 280
-                            });
-                            if (ani) {
-                                setTimeout(async () => {
-                                    ani.onfinish = () => {
-                                        this.showOfflineToast = false;
-                                    };
-                                    await ani.reverse();
-                                }, this.offlineToastDuration);
-                            }
-                        }
-                    }
-                }
-                reg.onupdatefound = async () => {
-                    let newWorker = reg.installing;
-                    if (newWorker) {
-                        newWorker.onstatechange = () => {
-                            if ((newWorker === null || newWorker === void 0 ? void 0 : newWorker.state) === "installed") {
-                                this.dispatchEvent(new Event("pwaUpdate"));
-                            }
-                        };
-                    }
-                };
-            }
-        }
-        this.setupEvents();
-    }
-    setupEvents() {
-        this.addEventListener('pwaUpdate', async () => {
-            if (navigator.serviceWorker) {
-                this.swreg = await navigator.serviceWorker.getRegistration();
-                if (this.swreg && this.swreg.waiting) {
-                    this.readyToAsk = true;
-                }
-            }
-        });
-    }
-    doUpdate() {
-        var _a, _b;
-        (_b = (_a = this.swreg) === null || _a === void 0 ? void 0 : _a.waiting) === null || _b === void 0 ? void 0 : _b.postMessage({ type: this.updateevent });
-        window.location.reload();
-    }
-    formatBytes(bytes, decimals = 2) {
-        if (bytes === 0)
-            return '0 Bytes';
-        const k = 1024;
-        const dm = decimals < 0 ? 0 : decimals;
-        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-        const i = Math.floor(Math.log(bytes) / Math.log(k));
-        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    _updateShow(event) {
+        const detailEvent = event;
+        this.show = detailEvent.detail;
     }
     render() {
         return html `
-      <div>
-       ${this.readyToAsk ? html `
-           <div id="updateToast" part="updateToast">
-             <span part="updateToastMessage">${this.updatemessage}</span>
-
-             <button part="updateToastButton" @click="${() => this.doUpdate()}">Update</button>
-           </div>
-         ` : null}
-
-      ${this.showOfflineToast ? html `
-          <div id="storageToast" part="offlineToast">
-            Ready to use Offline
-
-            ${this.showStorageEstimate ? html `<span id="storageEstimate">Cached ${this.storageUsed}</span>` : null}
-          </div>
-        ` : null}
-      </div>
+    <div id="updateToast" part="updateToast" show=${this.show}>
+      <span part="updateToastMessage">${this.updatemessage}</span>
+      <pwa-update-available @pwa-update-available=${this._updateShow}>
+        <button part="updateToastButton">Update</button>
+      </pwa-update-available>
+    </div>
     `;
     }
 };
 __decorate([
     property({ type: String })
-], pwaupdate.prototype, "swpath", void 0);
-__decorate([
-    property({ type: String })
-], pwaupdate.prototype, "updateevent", void 0);
-__decorate([
-    property({ type: String })
 ], pwaupdate.prototype, "updatemessage", void 0);
 __decorate([
     property({ type: Boolean })
-], pwaupdate.prototype, "readyToAsk", void 0);
+], pwaupdate.prototype, "show", void 0);
 __decorate([
-    property({ type: Boolean })
-], pwaupdate.prototype, "showStorageEstimate", void 0);
-__decorate([
-    property({ type: Boolean })
-], pwaupdate.prototype, "showOfflineToast", void 0);
-__decorate([
-    property({ type: Number })
-], pwaupdate.prototype, "offlineToastDuration", void 0);
-__decorate([
-    property({ type: String })
-], pwaupdate.prototype, "storageUsed", void 0);
+    eventOptions({ passive: true })
+], pwaupdate.prototype, "_updateShow", null);
 pwaupdate = __decorate([
     customElement("pwa-update")
 ], pwaupdate);
